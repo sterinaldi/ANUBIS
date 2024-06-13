e6437ef1087ea70985a82dbe3e27dcd855d05df8
sterinaldi <stefano.rinaldi@uni-heidelberg.de>
2024-06-13 18:02:27 +0200

    porting to models structure

--- anubis/utils.py
+++ anubis/utils.py
@@ -40,19 +40,30 @@ def get_samples_and_weights(draws):
     """
     return np.block([get_samples(draws), get_weights(draws)])
 
-def get_labels(draws, kind, pars_labels = None, par_models_labels = None):
+def get_labels(draws, kind, models = None):
     """
     Returns a list of labels for parameters and/or weights.
     
     Arguments:
         :iterable draws:               instances of het_mixture class
-        :str kind:                     whether to produce labels for plots ('pars', 'weights', 'joint') or for output ('txt')
-        :list-of-str pars_labels:      labels for parameters
-        :list-of-str par_model_labels: labels for models (for weights)
+        :str kind:                     whether to produce labels for plots ('pars', 'weights', 'joint') or for output ('save')
     
     Return:
         :list-of-str: labels
     """
+    if models is not None:
+        all_pars_names  = [lab for model in models for lab in model['par_names']]
+        all_pars_labels = [lab for model in models for lab in model['par_labels']]
+        # Fancy way of getting all unique instances while preserving order
+        unique_names  = list(dict.fromkeys(all_pars_names).keys())
+        unique_labels = list(dict.fromkeys(all_pars_names).keys())
+        # Identify items appearing once
+        d_names = {}
+        for i in all_pars_names: d_names[i] = i in d_names
+        # List of names and labels
+        pars_names        = [k for k in all_pars_names if not d_names[k]] + [k for k in unique_names if d_names[k]]
+        pars_labels       = [x for x, k in zip(all_pars_labels, all_pars_names) if not d_names[k]] + [x for x, k in zip(unique_labels, unique_names) if d_names[k]]
+        par_models_labels = [model['name'] for model in models]
     # Number of parameters and models
     if draws[0].n_shared_pars > 0:
         shared_pars_idx = -draws[0].n_shared_pars
@@ -61,16 +72,16 @@ def get_labels(draws, kind, pars_labels = None, par_models_labels = None):
     n_parameters  = int(np.sum([len(m.pars[:shared_pars_idx]) for m in draws[0].models[draws[0].augment:]]) + draws[0].n_shared_pars)
     n_par_models  = len(draws[0].models)-draws[0].augment
     # Parameter labels
-    if pars_labels is None or not (len(pars_labels) == n_parameters):
+    if models is None:
         parameter_labels = ['$\\theta_{0}$'.format(i+1) for i in range(n_parameters)]
     else:
         parameter_labels = ['${0}$'.format(l) for l in pars_labels]
-    # Model lalbels
-    if par_models_labels is None or not (len(par_models_labels) == n_par_models):
+    # Model labels
+    if models is None:
         weights_labels = ['$w_{'+'{0}'.format(i+1)+'}$' for i in range(n_par_models)]
     else:
         weights_labels = ['$w_\\mathrm{'+'{0}'.format(l)+'}$' for l in par_models_labels]
-    if np.array([d.augment for d in draws]).all():
+    if draws[0].augment:
         weights_labels = ['$w_\\mathrm{np}$'] + weights_labels
     # Decide what to return
     if kind == 'pars':
@@ -79,10 +90,10 @@ def get_labels(draws, kind, pars_labels = None, par_models_labels = None):
         return weights_labels
     elif kind == 'joint':
         return parameter_labels + weights_labels
-    elif kind == 'txt':
+    elif kind == 'save':
         # Remove LaTeX characters from string
-        labels = [s.translate(str.maketrans({st:'' for st in '$\{}'})) for s in parameter_labels + weights_labels]
+        labels = par_names + [s.translate(str.maketrans({st:'' for st in '$\{}'})) for s in weights_labels]
         labels = [s.replace('mathrm', '') for s in labels]
         return labels
     else:
-        raise ANUBISException("Kind not supported. Please pick from ['pars', 'weights', 'joint', 'txt']")
+        raise ANUBISException("Kind not supported. Please pick from ['pars', 'weights', 'joint', 'save']")
